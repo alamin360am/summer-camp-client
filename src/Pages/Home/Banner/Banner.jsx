a8a7695bfc4a378f1b8db5ec8d4c221f499f1c5d
alamin360am <alaminhimu0@gmail.com>
2023-06-11 17:20:54 +0600

    banner section responsive bug fixed

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -11,32 +11,32 @@ const Banner = () => {
       <div className="relative">
         <img src={img1} />
         <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-black backdrop:blur-sm bg-opacity-60 p-4 text-white">
-            <h2 className="text-3xl mb-4">Learn Badminton</h2>
-            <p className="mb-4">Learn Badminton in this summer. Be a pro player in Badminton</p>
+            <h2 className="text-xl md:text-3xl mb-4">Learn Badminton</h2>
+            <p className="mb-4 text-sm md:text-base">Learn Badminton in this summer. Be a pro player in Badminton</p>
             <button className="btn btn-primary bg-green-800 border-none hover:bg-green-950">Enroll Now</button>
         </div>
       </div>
       <div>
         <img src={img2} />
         <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-black backdrop:blur-sm bg-opacity-60 p-4 text-white">
-            <h2 className="text-3xl mb-4">Learn Cricket</h2>
-            <p className="mb-4">Learn Cricket in this summer. Be a pro player in Cricket</p>
+            <h2 className="text-xl md:text-3xl mb-4">Learn Cricket</h2>
+            <p className="mb-4 text-sm md:text-base">Learn Cricket in this summer. Be a pro player in Cricket</p>
             <button className="btn btn-primary bg-green-800 border-none hover:bg-green-950">Enroll Now</button>
         </div>
       </div>
       <div>
         <img src={img3} />
         <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-black backdrop:blur-sm bg-opacity-60 p-4 text-white">
-            <h2 className="text-3xl mb-4">Learn Football</h2>
-            <p className="mb-4">Learn Football in this summer. Be a pro player in Football</p>
+            <h2 className="text-xl md:text-3xl mb-4">Learn Football</h2>
+            <p className="mb-4 text-sm md:text-base">Learn Football in this summer. Be a pro player in Football</p>
             <button className="btn btn-primary bg-green-800 border-none hover:bg-green-950">Enroll Now</button>
         </div>
       </div>
       <div>
         <img src={img4} />
         <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-black backdrop:blur-sm bg-opacity-60 p-4 text-white">
-            <h2 className="text-3xl mb-4">Learn Voliball</h2>
-            <p className="mb-4">Learn Voliball in this summer. Be a pro player in Voliball</p>
+            <h2 className="text-xl md:text-3xl mb-4">Learn Voliball</h2>
+            <p className="mb-4 text-sm md:text-base">Learn Voliball in this summer. Be a pro player in Voliball</p>
             <button className="btn btn-primary bg-green-800 border-none hover:bg-green-950">Enroll Now</button>
         </div>
       </div>
