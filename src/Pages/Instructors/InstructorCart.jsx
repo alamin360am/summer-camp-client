9a1ed98210018c47eec3cd201b398c5519514597
alamin360am <alaminhimu0@gmail.com>
2023-06-14 14:39:25 +0600

    instructor done

---
+++ src/Pages/Instructors/InstructorCart.jsx
@@ -0,0 +1,15 @@
+const InstructorCart = ({ instructor }) => {
+  const { name, photoUrl, email, numberOfStudents } = instructor;
+  return (
+    <div className="flex flex-col items-center bg-white p-4 rounded-lg">
+      <figure className="w-40 h-40 rounded-full overflow-hidden mb-4 flex justify-center items-center">
+        <img src={photoUrl} alt="" className="w-full h-full rounded-lg" />
+      </figure>
+      <h3 className="text-2xl font-semibold text-black mb-2">{name}</h3>
+      <p><span className="text-black">Email: </span>{email}</p>
+      <p><span className="text-black">Students: </span>{numberOfStudents}</p>
+    </div>
+  );
+};
+
+export default InstructorCart;
--- src/Pages/Instructors/Instructors.jsx
+++ src/Pages/Instructors/Instructors.jsx
@@ -1,12 +1,28 @@
+import { useEffect, useState } from "react";
+import Heading from "../../Component/Heading/Heading";
 import useTitle from "../../Hook/useTitle";
+import InstructorCart from "./InstructorCart";
 
 const Instructors = () => {
-    useTitle("Our Instructors")
-    return (
-        <section className="my-10">
-            <h2 className="text-2xl">instructors</h2>
-        </section>
-    );
+  useTitle("Our Instructors");
+  const [instructors, setInstructors] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/instructor")
+      .then((res) => res.json())
+      .then((data) => setInstructors(data));
+  }, []);
+
+  return (
+    <section className="pt-28">
+      <Heading title={"Our Instructor"}></Heading>
+      <div className="grid gird-cols-1 md:grid-cols-3 gap-8 mb-8">
+        {
+            instructors.map(instructor => <InstructorCart key={instructor._id} instructor={instructor}></InstructorCart>)
+        }
+      </div>
+    </section>
+  );
 };
 
 export default Instructors;
